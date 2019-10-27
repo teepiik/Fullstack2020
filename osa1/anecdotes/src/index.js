bd2d96b9b6751288a5ebc75f036b3135cbac2a36
teepiik <teemu.piik@gmail.com>
2019-10-27 13:52:54 +0200

    1.12 - 1.14

--- osa1/anecdotes/src/index.js
+++ osa1/anecdotes/src/index.js
@@ -1,12 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Button = (props) => {      
+    return (
+        <button onClick={props.onClick}> {props.label} </button>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Anecdote = (props) => {
+    return (
+        <div>
+            <h2>{props.title}</h2>
+            <p>{props.anecdotes[props.index]}</p>
+            <p>Has {props.points[props.index]} votes</p>
+        </div>    
+    )
+}
+
+const App = (props) => {
+    const [selected, setSelected] = useState(0)
+    const [mostVotes, setMostVotes] = useState(0)
+    const [points, setPoints] = useState(Array(props.anecdotes.length).fill(0))
+
+    const handleNextAnecdote = () => {
+        setSelected(Math.floor(Math.random()*props.anecdotes.length))
+    }
+
+    const handleVote = () => {
+        const copy = [...points]
+        copy[selected] += 1
+        setPoints(copy)
+        if(points[selected]>=points[mostVotes]) {
+            setMostVotes(selected)
+        }
+    }
+
+  return (
+        <div>
+            <Anecdote anecdotes={props.anecdotes} title = "Anecdote of the day" index={selected} points={points}/>
+            <Button onClick = {handleVote} label = "Vote"/>
+            <Button onClick = {handleNextAnecdote} label = "Next Anecdote"/>
+            <Anecdote anecdotes={props.anecdotes} title = "Most popular anecdote" index={mostVotes} points={points}/>
+        </div>
+    )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
