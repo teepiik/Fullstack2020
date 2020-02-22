e3ccd43e0280d5ccb5b5eeb3a6af79fa4d693868
teepiik <teemu.piik@gmail.com>
2020-02-22 15:30:11 +0200

    5.16 still not working

--- osa5/bloglist-frontend/src/components/BlogForm.test.js
+++ osa5/bloglist-frontend/src/components/BlogForm.test.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { render, fireEvent } from '@testing-library/react'
 import BlogForm from './BlogForm'
 // 5.16
@@ -6,24 +6,49 @@ import BlogForm from './BlogForm'
 test('BlogForm sends correct data.', () => {
     const createBlog = jest.fn()
 
+    /* Not working
+    const [title, setTitle] = useState([''])
+    const [author, setAuthor] = useState([''])
+    const [url, setUrl] = useState([''])
+    */
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value)
+    }
+
+    const handleAuthorChange = (event) => {
+        setAuthor(event.target.value)
+    }
+
+    const handleUrlChange = (event) => {
+        setUrl(event.target.value)
+    }
+
     const component = render(
-        <BlogForm handleNewBlog={createBlog}/>
+        <BlogForm
+            handleNewBlog={createBlog}
+            title={title}
+            author={author}
+            url={url}
+            handleTitleChange={handleTitleChange}
+            handleAuthorChange={handleAuthorChange}
+            handleUrlChange={handleUrlChange}
+        />
     )
 
-    const title = component.container.querySelector('#title')
-    const author = component.container.querySelector('#author')
-    const url = component.container.querySelector('#url')
+    const titleComp = component.container.querySelector('#title')
+    const authorComp = component.container.querySelector('#author')
+    const urlComp = component.container.querySelector('#url')
     const form = component.container.querySelector('#form')
 
-    fireEvent.change(title, {
+    fireEvent.change(titleComp, {
         target: { value: 'What is love' }
     })
 
-    fireEvent.change(author, {
+    fireEvent.change(authorComp, {
         target: { value: 'Haddaway' }
     })
 
-    fireEvent.change(url, {
+    fireEvent.change(urlComp, {
         target: { value: 'greatesthits.com' }
     })
 
