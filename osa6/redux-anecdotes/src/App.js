604d490fde67d5ca08e35d4417c20900d4145494
teepiik <teemu.piik@gmail.com>
2020-02-26 20:29:51 +0200

    6.3, 6.4, 6.6, 6.7, 6.8 done

---
+++ osa6/redux-anecdotes/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import AnecdoteList from './components/AnecdoteList'
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+    return (
+        <div>
+            <Notification />
+            <h2>Anecdotes</h2>
+            <AnecdoteList />
+            <AnecdoteForm />
+        </div>
+    )
+}
+
+export default App
