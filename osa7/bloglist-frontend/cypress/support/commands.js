e656cd852224b9b1f8d1a82dff6337bf639a6135
teepiik <teemu.piik@gmail.com>
2020-03-06 10:52:25 +0200

    cleanup

--- osa7/bloglist-frontend/cypress/support/commands.js
+++
@@ -1,8 +0,0 @@
-Cypress.Commands.add('login', ({ username, password }) => {
-    cy.request('POST', 'http://localhost:3003/api/login', {
-        username, password
-    }).then(({ body }) => {
-        localStorage.setItem('loggedUser', JSON.stringify(body))
-        cy.visit('http://localhost:3000')
-    })
-})
