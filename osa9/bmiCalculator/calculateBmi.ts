9f19aff6973ccbc11013136e5fe86a5ff55c9fd6
teepiik <teemu.piik@gmail.com>
2020-06-06 12:05:07 +0300

    9.5 done

--- osa9/bmiCalculator/calculateBmi.ts
+++ osa9/bmiCalculator/calculateBmi.ts
@@ -1,41 +1,28 @@
-interface BmiValues {
-    weight: number
-    height: number
-}
 
-// To validate input
-const parseArguments = (args: Array<string>): BmiValues => {
-    if (args.length < 4) throw new Error('Not enough arguments')
-    if (args.length > 4) throw new Error('Too many arguments')
-
-    if(!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-        return {
-            weight: Number(args[2]),
-            height: Number(args[3])
-        }
-    } else {
-        throw new Error('Incorrect arguments, give weight and height')
-    }
-}
-
-const calculateBmi = (height: number, weight: number) => {
-
+const calculate = (height: number, weight: number) => {
     const bmi = (weight / Math.pow((height/100),2))
     console.log('Bmi: ', bmi)
+
+    if(isNaN(bmi)) {
+        return 'Malformatted parameters'
+    }
+
     if (bmi < 18.5) {
-        console.log('underweight')
+        return 'underweight'
     } else if (bmi >= 18.5 && bmi <= 25 ) {
-        console.log('normal weight')
+        return 'normal weight'
     } else if (bmi > 25 && bmi <= 30 ) {
-        console.log('overweight')
+        return 'overweight'
     } else {
-        console.log('obese')
+        return 'obese'
     }
 }
 
-try {
-    const { weight, height } = parseArguments(process.argv)
-    calculateBmi( weight, height )
-} catch (e) {
-    console.log('Error: ', e.message)
+export const calculateBmi = (height: number, weight: number) => {
+    try {
+        return calculate( height, weight )
+        
+    } catch (e) {
+        return 'Error' + e.message
+    }
 }
--- osa9/bmiCalculator/index.js
+++ osa9/bmiCalculator/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
+const { calculateBmi } = require('./calculateBmi')
 
 app.get('/hello', (req, res) => {
     res.send('Hello Full Stack!')
+})
+
+app.get('/bmi', (req, res) => {
+    const weight = req.query.weight
+    const height = req.query.height
+    res.send(calculateBmi(height, weight))
 })
 
 const PORT = 3003
